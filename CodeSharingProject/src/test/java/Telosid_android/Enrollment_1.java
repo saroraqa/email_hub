811320dcd2d8dc6bd44f0d9b7c45ba12de685b11
anshul <anshul@Anshul-PC.fios-router.home>
2017-04-25 12:19:33 -0400

    moving to code sharing project

---
+++ CodeSharingProject/src/test/java/Telosid_android/Enrollment_1.java
@@ -0,0 +1,15 @@
+package Telosid_android;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Enrollment_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+         driver.get("https://www.gotomeeting.com/");
+         
+         
+	}
+
+}
