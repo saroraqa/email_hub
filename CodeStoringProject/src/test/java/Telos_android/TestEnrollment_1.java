1b1255befaf66cd625f951084eff309517bd07a1
anshul <anshul@Anshul-PC.fios-router.home>
2017-04-25 13:14:41 -0400

    moving changes to Code Sharing project

--- CodeStoringProject/src/test/java/Telos_android/TestEnrollment_1.java
+++ CodeStoringProject/src/test/java/Telos_android/TestEnrollment_1.java
@@ -1,15 +1,16 @@
-package Telosid_android;
+package Telos_android;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Enrollment_1 {
+public class TestEnrollment_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		ChromeDriver driver = new ChromeDriver();
-         driver.get("https://www.gotomeeting.com/");
-         
-         
+		driver.get("https://www.gotomeeting.com/");
+		 
+		 System.out.println();
+	}
 	}
 
-}
